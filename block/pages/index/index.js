4a5f50fa580b1d050f14b5ceb72fad34a8c22051
longwen <1348551496@qq.com>
2018-08-06 22:44:02 +0800

    update block chain meeting

---
+++ block/pages/index/index.js
@@ -0,0 +1,153 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    recommend: [
+      {
+        title: '2018区块链世界峰会',
+        address: '北京 朝阳',
+        date: '12-23 周五'
+      },
+      {
+        title: '2017区块链金融世界',
+        address: '北京 昌平',
+        date: '08-23 周五'
+      },
+      {
+        title: '2016区块链制造汽车',
+        address: '北京 大兴',
+        date: '06-23 周五'
+      }, {
+        title: '2016区块链以太坊币',
+        address: '北京 海淀',
+        date: '07-26 周五'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  clickA() {
+    console.log('click container-A')
+  },
+  clickB() {
+    console.log('click container-B')
+  },
+  clickC() {
+    console.log('click container-C')
+  },
+  clickEvent(ev) {
+    console.log('ev', ev)
+  },
+})
+
+
+// //index.js
+// //获取应用实例
+// const app = getApp()
+
+// Page({
+//   data: {
+//     motto: 'Hello World',
+//     userInfo: {},
+//     hasUserInfo: false,
+//     canIUse: wx.canIUse('button.open-type.getUserInfo')
+//   },
+//   //事件处理函数
+//   bindViewTap: function() {
+//     wx.navigateTo({
+//       url: '../logs/logs'
+//     })
+//   },
+//   onLoad: function () {
+//     if (app.globalData.userInfo) {
+//       this.setData({
+//         userInfo: app.globalData.userInfo,
+//         hasUserInfo: true
+//       })
+//     } else if (this.data.canIUse){
+//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+//       // 所以此处加入 callback 以防止这种情况
+//       app.userInfoReadyCallback = res => {
+//         this.setData({
+//           userInfo: res.userInfo,
+//           hasUserInfo: true
+//         })
+//       }
+//     } else {
+//       // 在没有 open-type=getUserInfo 版本的兼容处理
+//       wx.getUserInfo({
+//         success: res => {
+//           app.globalData.userInfo = res.userInfo
+//           this.setData({
+//             userInfo: res.userInfo,
+//             hasUserInfo: true
+//           })
+//         }
+//       })
+//     }
+//   },
+//   getUserInfo: function(e) {
+//     console.log(e)
+//     app.globalData.userInfo = e.detail.userInfo
+//     this.setData({
+//       userInfo: e.detail.userInfo,
+//       hasUserInfo: true
+//     })
+//   }
+// })
